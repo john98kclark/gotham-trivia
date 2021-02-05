1e9d27d477c68535b6fe0e18f27c34342bd72cdd
DESKTOP-IJDUK3A\Admin <john98kclark@gmail.com>
2021-02-04 20:05:29 -0500

    Creating scriptable objects

--- unity gotham/Assets/GameManager.cs
+++ unity gotham/Assets/GameManager.cs
@@ -5,14 +5,13 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     int m_CurrentQuestion = 0;
-    [TextArea(1,5)]
-    [SerializeField] string[] m_Questions;
+   
    [SerializeField] Text m_QuestionText;
     // Start is called before the first frame update
     void Start()
     {
         //Setting the current question from our m_Questions array to the m_QuestionsText component in the scene.
-        m_QuestionText.text = m_Questions[m_CurrentQuestion];
+        //m_QuestionText.text = m_Questions[m_CurrentQuestion];
         
         Debug.Log("Hello Gotham");
     }
---
+++ unity gotham/Assets/ScriptableObjects/Questions/QuestionObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionObject : ScriptableObject
+{
+    [TextArea(1, 5)]
+    [SerializeField] string m_Question;
+
+
+}
