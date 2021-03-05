55c0fb5fcb93f9801519f0789593c62be82ba789
Kyle Conibear <kyle.a.conibear@gmail.com>
2021-03-04 21:05:42 -0500

    Update GameManager.cs

--- unity gotham/Assets/Scripts/GameManager.cs
+++ unity gotham/Assets/Scripts/GameManager.cs
@@ -3,43 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
-    [SerializeField]
-    Question m_Questions;
+public class GameManager : MonoBehaviour {
+	[SerializeField]
+	Question m_Questions;
 
-    [SerializeField]
-    private QuestionData[] m_QuestionsData;
-    private int m_CurrentQuestion = 0;//m_CurrentQuestion = index = element
-    #region properties
+	[SerializeField]
+	private QuestionData[] m_QuestionsData;
 
-    private bool IsThereANextQuestion
-    {
-        get
-        {
-            return m_CurrentQuestion >= m_QuestionsData.Length - 1;
-        }
-    }
-
-    #endregion
-    #region MonoBehaviour Methods
-
-    private void Start()
-    {
-        AnswerButton.OnAnswered = GoToNextQuestion;
-        m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
-    }
-    #endregion
-    #region
-    private void GoToNextQuestion()
-    {
-        if (IsThereANextQuestion)
-        {
-            m_CurrentQuestion++;
-            m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
-        }
-    }
+	private int m_CurrentQuestion = 0; //m_CurrentQuestion = index = element
 
 
-    #endregion
+	#region properties
+
+	private bool IsThereANextQuestion {
+		get { return m_CurrentQuestion < m_QuestionsData.Length - 1; }
+	}
+
+	#endregion
+
+
+	#region MonoBehaviour Methods
+
+	private void Start() {
+		AnswerButton.OnAnswered = GoToNextQuestion;
+		m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
+	}
+
+	#endregion
+
+
+	#region
+
+	private void GoToNextQuestion() {
+		if (IsThereANextQuestion) {
+			m_CurrentQuestion++;
+			m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
+		}
+	}
+
+	#endregion
 }
