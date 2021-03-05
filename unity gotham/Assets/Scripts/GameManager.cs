6c8002193f6dbbcd3426cd72f5943facfcc8ea4c
DESKTOP-IJDUK3A\Admin <john98kclark@gmail.com>
2021-03-04 20:35:58 -0500

    Polished fiest Question

--- unity gotham/Assets/Scripts/GameManager.cs
+++ unity gotham/Assets/Scripts/GameManager.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
-	[SerializeField]
-	Question m_Questions;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    Question m_Questions;
 
-	[SerializeField]
-	private QuestionData[] m_QuestionsData;
-	private int m_CurrentQuestion = 0;
+    [SerializeField]
+    private QuestionData[] m_QuestionsData;
+    private int m_CurrentQuestion = 0;//m_CurrentQuestion = index = element
+    #region properties
 
-	#region MonoBehaviour Methods
+    private bool IsThereANextQuestion
+    {
+        get
+        {
+            return m_CurrentQuestion >= m_QuestionsData.Length - 1;
+        }
+    }
 
-	private void Start() {
-		AnswerButton.OnAnswered=GoToNextQuestion;
-		m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
-	}
+    #endregion
+    #region MonoBehaviour Methods
+
+    private void Start()
+    {
+        AnswerButton.OnAnswered = GoToNextQuestion;
+        m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
+    }
     #endregion
     #region
     private void GoToNextQuestion()
     {
-		m_CurrentQuestion++;
-		m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
-	}
-
+        if (IsThereANextQuestion)
+        {
+            m_CurrentQuestion++;
+            m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
+        }
+    }
 
 
     #endregion
