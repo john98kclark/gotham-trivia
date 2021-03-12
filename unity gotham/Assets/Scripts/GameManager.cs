43e2307f4dc257e1f4990a27d73f40c9a1789b91
DESKTOP-IJDUK3A\Admin <john98kclark@gmail.com>
2021-03-11 22:27:09 -0500

    question 2

--- unity gotham/Assets/Scripts/GameManager.cs
+++ unity gotham/Assets/Scripts/GameManager.cs
@@ -3,43 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
-	[SerializeField]
-	Question m_Questions;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    Question m_Questions;
 
-	[SerializeField]
-	private QuestionData[] m_QuestionsData;
+    [SerializeField]
+    private QuestionData[] m_QuestionsData;
 
-	private int m_CurrentQuestion = 0; //m_CurrentQuestion = index = element
+    private int m_CurrentQuestion = 0; //m_CurrentQuestion = index = element
+    private IEnumerator m_GoToNextQuestionCoroutine;
 
+    #region properties
 
-	#region properties
+    private bool IsThereANextQuestion
+    {
+        get { return m_CurrentQuestion < m_QuestionsData.Length - 1; }
+    }
 
-	private bool IsThereANextQuestion {
-		get { return m_CurrentQuestion < m_QuestionsData.Length - 1; }
-	}
-
-	#endregion
-
-
-	#region MonoBehaviour Methods
-
-	private void Start() {
-		AnswerButton.OnAnswered = GoToNextQuestion;
-		m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
-	}
-
-	#endregion
+    #endregion
 
 
-	#region
+    #region MonoBehaviour Methods
 
-	private void GoToNextQuestion() {
-		if (IsThereANextQuestion) {
-			m_CurrentQuestion++;
-			m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
-		}
-	}
+    private void Start()
+    {
 
-	#endregion
+        AnswerButton.OnAnswered = GoToNextQuestion;
+        m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
+    }
+
+    #endregion
+
+
+    #region
+
+    private void GoToNextQuestion()
+    {
+        if (IsThereANextQuestion && m_GoToNextQuestionCoroutine == null)
+        {
+            m_GoToNextQuestionCoroutine = GoToNextQuestionCoroutine();
+            StartCoroutine(m_GoToNextQuestionCoroutine);
+        }
+    }
+    private IEnumerator GoToNextQuestionCoroutine()
+    {
+        //A function that can yield when others cant
+        yield return new WaitForSecondsRealtime(5);
+        m_CurrentQuestion++;
+        m_Questions.Initialize(m_QuestionsData[m_CurrentQuestion]);
+        m_GoToNextQuestionCoroutine = null;
+    }
+
+
+    #endregion
 }
